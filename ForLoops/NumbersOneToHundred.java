9eb4f8c7b0fbe422e335f4d98364efde0238cf6a
Yordan Nikolov <x-normalize@protonmail.com>
2023-06-28 09:04:11 +0300

    Create NumbersOneToHundred.java

---
+++ ForLoops/NumbersOneToHundred.java
@@ -0,0 +1,15 @@
+package ForLoops;
+
+import java.util.Scanner;
+
+public class NumbersOneToHundred {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+
+
+    }
+}
