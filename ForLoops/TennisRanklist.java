82d0607ffc2eb4d1ee9681addd022a292d2ab84f
Yordan Nikolov <x-normalize@protonmail.com>
2023-07-04 09:21:28 +0300

    Create TennisRanklist.java

---
+++ ForLoops/TennisRanklist.java
@@ -0,0 +1,13 @@
+package ForLoops;
+
+import java.util.Scanner;
+
+public class TennisRanklist {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
+}
